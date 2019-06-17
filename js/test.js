281905561ac329ad1ae98390bdc792cdf6697146
obdegirmenci <obdegirmenci@outlook.com>
2019-06-17 18:33:43 +0300

    Update test.js
    Sayfa içine JS ile mesaj yazdırma denemesi.

--- js/test.js
+++ js/test.js
@@ -6,3 +6,12 @@ $().ready(function() {
 });
 
 */
+
+// Test. Bu dosya çalışıyor
+// alert(msg1);
+
+
+// Bu kod çalışıyor
+var msg1 = "Batsın bu dünya...";
+
+document.querySelector("main").innerHTML = "<p>" + msg1 + "</p>";
