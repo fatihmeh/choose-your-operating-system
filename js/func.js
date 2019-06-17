f01db1b797e9493d706993a54c95a8013e06926f
obdegirmenci <obdegirmenci@outlook.com>
2019-06-18 00:04:39 +0300

    Master merge öncesi düzenleme

---
+++ js/func.js
@@ -0,0 +1,51 @@
+var msg1 = "Batsın bu dünya...";
+
+document.querySelector("main").innerHTML = "<p>" + msg1 + "</p>";
+
+/*
+
+//// Taslak ////
+
+sorular{
+   s1 = Kimi seven?
+   c1 = Onu
+   c2 = Bunu
+   c3 = Şunu
+
+   s2 = Nabıyon beya?
+   c4 = İyi beya
+   c5 = Nolsun beya
+   c6 = Eyi sen nabıyon beya
+}
+sonuclar{
+   SA
+   WinSA
+   MacSA
+   SASA
+   ...
+   ...
+}
+
+
+Sayfa yüklendiğinde çalıştır > kontrol_fonksiyonu()
+
+Cevaba tıklandığında çalıştır > kontrol_fonksiyonu(cevabın id'si)
+
+kontrol_fonksiyonu(id) {
+
+      1. cevapSA { şimdikini soruyu sil, yönlendirdiği soruyu göster }
+      2. cevapSA {...}
+      3. cevapSA {sil, 2'ye dön}
+      4. cevapSA {...}
+      5. cevapSA {sil, 5'in sonucu göster (WinSA)}
+      6. cevapSA {...}
+      7. cevapSA {...}
+      8. cevapSA {sil, 8'in sonucu göster (MacSA)}
+      "  "  "
+      "  "  "
+      "  "  "
+
+      hiç biri değise { ilk soruyu sor (Başlangıç) }
+}
+
+*/
