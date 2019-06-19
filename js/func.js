68d03a15feb2932941afd2b994eda669ce9ff74f
obdegirmenci <obdegirmenci@outlook.com>
2019-06-19 21:28:14 +0300

    Sorular yazdırıldı
    JSON dosyasında duran sorular, ilgili cevap seçenekleri, başka soruya yönlendirmeler, ve  son aşamadaki sonuçları ufak bir döngü ile HTML içerisine yerleştirildi.

--- js/func.js
+++ js/func.js
@@ -1,6 +1,6 @@
 var msg1 = "Batsın bu dünya...";
 
-document.querySelector("main").innerHTML = "<p>" + msg1 + "</p>";
+document.querySelector("main").innerHTML = "<p style='color:slategray'><b>" + msg1 + "</b></p>";
 
 /*
 
@@ -52,20 +52,37 @@ kontrol_fonksiyonu(id) {
 
 var userlang = "tr";
 
+// JSON dosyası son hâlinde değil, anahtar ve değer ağacı daha sonradan değiştirilebilir
+
+// Json dosyasını istiyoruz
 var request = new XMLHttpRequest();
 request.open("GET","js/lang/"+userlang+".json");
 request.responseType = 'json';
 request.send();
 
+// Sonucu alıyoruz ve ekrana göstermek için fonksiyona yolluyoruz
 request.onload = function() {
-   var soru = request.response;
-   sorubas(soru);
+   var jsonRespond = request.response;
+   createQuestions(jsonRespond);
  }
 
-function sorubas(jsonObj){
-   var count = Object.keys(jsonObj.questions).length;
-   console.log("Toplam soru: " + count);
-   for (i = 0; i < count; i++) {
-      console.log(jsonObj.questions[i]);
+function createQuestions(jsonObj){
+
+   // Soruların adetini hesaplayıp döngüyü adet kadar çalıştırıyoruz
+   for (i = 0; i < Object.keys(jsonObj.questions).length; i++) {
+
+      // Bu fonksiyon ile seçilen cevap başka soruya mı, yoksa direkt bir sonuca mı götürüyor onu kontrol ediyoruz ve ona uygun gösterimleri sağlıyoruz
+      var where;
+      function tocheck(par1) {
+         par1.to !== "" ? where = " <b>(" + par1.to + "</b> numaraya git)" :  where = " <b>(Sonuç ID: " + par1.r + " <span style='color:dodgerblue'>" + jsonObj.results[par1.r].s + "</span></b>)";
+         return where;
+      }
+      
+      // HTML içinde gerekli elementleri oluşturup JSON'dan gelen verileri uygun kısımlara yerleştiriyoruz
+      var elemType = document.createElement("p");
+      elemType.innerHTML = "<b>Soru ID: " + jsonObj.questions[i].id + "</b> " + jsonObj.questions[i].q + "<br><b>Cevaplar: </b>" + jsonObj.questions[i].a1[0].t + tocheck(jsonObj.questions[i].a1[0]) +  " / " + jsonObj.questions[i].a2[0].t + tocheck(jsonObj.questions[i].a2[0]);
+      document.querySelector("main").appendChild(elemType);
    }
+   
+   
 }
